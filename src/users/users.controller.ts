a1c9839101ccb3940ef35e1f674813bc9e16ea58
Abdullah Al Noman <Abdullah.Noman@dizishore.com>
2025-04-09 11:46:57 +0600

    create user with validation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import { CreateUserDTO } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  constructor(private userService: UsersService) {}
+  @Post('/signup')
+  @HttpCode(HttpStatus.CREATED)
+  async createUser(@Body() body: CreateUserDTO): Promise<User> {
+    try {
+      return this.userService.create(body.email, body.password);
+    } catch (error) {
+      return error;
+    }
+  }
+}
