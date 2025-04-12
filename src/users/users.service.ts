588ced96263fc84a33e38ce59081915293f522ff
Abdullah Al Noman <Abdullah.Noman@dizishore.com>
2025-04-12 12:21:21 +0600

    added findOne and find

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,10 +5,24 @@ import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+  constructor(
+    @InjectRepository(User) private readonly repo: Repository<User>,
+  ) {}
 
   async create(email: string, password: string) {
     const user = this.repo.create({ email, password });
     return await this.repo.save(user);
+  }
+  async findOne(id: number) {
+    return await this.repo.findOne({
+      where: { id },
+    });
+  }
+  async find(email: string) {
+    return this.repo.find({
+      where: {
+        email: email,
+      },
+    });
   }
 }
