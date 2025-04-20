c904df81d9b3eba168a51ab5cb38e575bef9a69c
Abdullah Al Noman <Abdullah.Noman@dizishore.com>
2025-04-20 15:49:13 +0600

    adee signup methode

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,6 +1,19 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Injectable()
-export class AuthService{
+export class AuthService {
+  constructor(private userService: UsersService) {}
 
+  async signup(email: string, password: string) {
+    const users = await this.userService.find(email);
+    if (users) {
+      console.log(
+        '%cError: %cEmail in use',
+        'color: red; font-weight: bold; font-size: 16px;',
+        'color: orange; font-style: italic;',
+      );
+      throw new BadRequestException('Email in use');
+    }
+  }
 }
