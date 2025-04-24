def3c0fa540e0568766fe7c5cf6a036eb3a65544
Abdullah Al Noman <Abdullah.Noman@dizishore.com>
2025-04-24 10:16:07 +0600

    signup with hash password

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,13 +1,17 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
-
+import { randomBytes, scrypt as _script } from 'crypto';
+import { promisify } from 'util';
+const scrypt = promisify(_script);
 @Injectable()
 export class AuthService {
   constructor(private userService: UsersService) {}
 
   async signup(email: string, password: string) {
     const users = await this.userService.find(email);
-    if (users) {
+    console.log(users);
+
+    if (users.length > 0) {
       console.log(
         '%cError: %cEmail in use',
         'color: red; font-weight: bold; font-size: 16px;',
@@ -15,5 +19,11 @@ export class AuthService {
       );
       throw new BadRequestException('Email in use');
     }
+    const salt = randomBytes(8).toString('hex');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    const resultPassword = salt + '.' + hash.toString('hex');
+    // nore creating a new user with this password
+    const user = await this.userService.create(email, resultPassword);
+    return user;
   }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,7 +7,6 @@ import {
   Query,
   Delete,
   Patch,
-
 } from '@nestjs/common';
 import { CreateUserDTO } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -15,28 +14,31 @@ import { User } from './user.entity';
 import { UpdateUserDTO } from './dtos/update-user.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDTO } from './dtos/user.dto';
+import { AuthService } from './auth.service';
 
 @Serialize(UserDTO)
 @Controller('auth')
 export class UsersController {
-  constructor(private userService: UsersService) {}
-  
+  constructor(
+    private userService: UsersService,
+    private autheService: AuthService,
+  ) {}
+
   @Post('/signup')
   createUser(@Body() body: CreateUserDTO): Promise<User> {
-    return this.userService.create(body.email, body.password);
+    return this.autheService.signup(body.email, body.password);
   }
-  
 
   @Get('/:id')
   getUserInfo(@Param('id') id: string) {
     return this.userService.findOne(parseInt(id));
   }
-  
+
   @Get()
   findAllUsers(@Query('email') email: string) {
     return this.userService.find(email);
   }
-  
+
   @Delete('/:id')
   removeUserById(@Param('id') id: string) {
     return this.userService.remove(parseInt(id));
